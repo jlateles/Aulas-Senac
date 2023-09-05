5b60a0bb4c89000e342603b8a10786da63cff526
Júlia Teles <juliatelesicm@gmail.com>
2023-09-05 09:59:15 -0300

    Create ex1.cs

---
+++ c# exercicios/ex1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+ class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine ("Nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Nome digitado: " + nome);
+        }
+    }
+}
