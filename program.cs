85d9519d9179e0324c6edf4c3f9f62dd84300258
Júlia Teles <juliatelesicm@gmail.com>
2023-09-25 16:05:53 -0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -6,13 +6,26 @@ namespace AulaUdemy{
 
         static void Main(string[] args){
 
-            Ducha objetoDucha = new Ducha {};
-            objetoDucha.Material = "plástico";
-            objetoDucha.Cor = "branca";
-            objetoDucha.Tamanho = "15";
-            objetoDucha.Potencia = "220";
+            Console.WriteLine("Digite o material: ")
+            string material = Console.ReadLine();
+            Console.WriteLine("Digite a cor: ");
+            string Cor = Console.ReadLine();
+            Console.WriteLine("Digite o tamanho: ");
+            string Tamanho = Console.ReadLine();
+            Console.WriteLine("Digite a potência: ");
+            string Potencia = Console.ReadLine();
+            Console.WriteLine("Digite o estado: ");
+            bool Estadp = Console.ReadLine();
+            Console.WriteLine("Digite os litros: ");
+            int Litros = Console.ReadLine();
 
+            //Ducha objetoDucha = new Ducha {};
+            // objetoDucha.Material = "plástico";
+            //objetoDucha.Cor = "branca";
+            // objetoDucha.Tamanho = "15";
+            //objetoDucha.Potencia = "220";
             objetoDucha.Jorrar();
+                
         }
     }
 }
